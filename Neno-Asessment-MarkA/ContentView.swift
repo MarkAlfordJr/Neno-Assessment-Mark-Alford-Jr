21e288771345f424dddd07b779a32934d5d78894
Mark Alford Jr <markalfordjr@gmail.com>
2023-03-15 11:00:10 -0400

    Initial Commit

---
+++ Neno-Asessment-MarkA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Neno-Asessment-MarkA
+//
+//  Created by Mark Alford on 3/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Neno-Asessment-MarkA/Neno_Asessment_MarkAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neno_Asessment_MarkAApp.swift
+//  Neno-Asessment-MarkA
+//
+//  Created by Mark Alford on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Neno_Asessment_MarkAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
